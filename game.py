51cf69833c9edf05cc53c4e32e883c9034aa5133
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2021-07-30 10:18:59 +0430

    draw 3*3 grid and opening images

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame as pg, sys
 from pygame.locals import *
 import time
 
-# Initialize global variables.
+# Initializing global variables
 XO = 'x'
 winner = None
 draw = False
@@ -13,7 +13,7 @@ line_color = (10, 10, 10)
 # Tic Tac Toe 3*3 board
 TTT = [[None]*3, [None]*3, [None]*3]
 
-# Initialize pygame window.
+# Initializing pygame window
 pg.init()
 fps = 30
 CLOCK = pg.time.Clock()
@@ -29,3 +29,21 @@ o_img = pg.image.load('images\O.png')
 opening_img = pg.transform.scale(opening_img, (width, height + 100))
 x_img = pg.transform.scale(x_img, (80, 80))
 o_img = pg.transform.scale(o_img, (80, 80))
+
+def game_opening():
+    screen.blit(opening_img, (0, 0))
+    pg.display.update()
+    time.sleep(1)
+    screen.fill(white)
+
+    # Drawing vertical lines
+    pg.draw.line(screen, line_color, (width / 3, 0), (width / 3, height), 7)
+    pg.draw.line(screen, line_color, (2 * width / 3, 0), (2 * width / 3, height), 7)
+    # Drawing horizontal lines
+    pg.draw.line(screen, line_color, (0, height / 3), (width, height / 3), 7)
+    pg.draw.line(screen, line_color, (0, 2 * height / 3), (width, 2 * height / 3), 7)
+    draw_status()
+
+
+
+    
