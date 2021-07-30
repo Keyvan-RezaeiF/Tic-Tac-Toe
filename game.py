2e5b1ae3752dd004b09a8f90f90a34b88c69b3e5
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2021-07-30 10:40:44 +0430

    check the winner

--- game.py
+++ game.py
@@ -63,4 +63,37 @@ def draw_status():
     screen.blit(text, text_rect)
     pg.display.update()
 
+def check_win():
+    global TTT, winner, draw
+
+    # Check for winning rows
+    for row in range(0, 3):
+        if (TTT[row][0] == TTT[row][1] == TTT[row][2]) and (TTT[row][0] is not None):
+            # This row won
+            winner = TTT[row][0]
+            pg.draw.line(screen, (255, 0, 0), (0, (row+1)*height/3-height/6),
+                            (width, (row+1)*height/3-height/6), 4)
+            break
+
+    # Check for winning columns
+    for col in range(0, 3):
+        if (TTT[0][col] == TTT[1][col] == TTT[2][col]) and (TTT[0][col] is not None):
+            winner = TTT[0][col]
+            pg.draw.line(screen, (255, 0, 0), ((col+1)*width/3-width/6, 0), 
+                            ((col+1)*width/3-width/6, height), 4)
+            break
+
+    # Check for diagonal winners
+    if (TTT[0][0] == TTT[1][1] == TTT[2][2]) and (TTT[0][0] is not None):
+        winner = TTT[0][0]
+        pg.draw.line(screen, (255, 70, 70), (50, 50), (350, 350), 4)
+
+    if (TTT[0][2] == TTT[1][1] == TTT[2][0]) and (TTT[0][2] is not None):
+        winner = TTT[0][2]
+        pg.draw.line(screen, (255, 70, 70), (50, 50), (350, 350), 4)
+
+    if (all([all(row) for row in TTT]) and winner is None):
+        draw = True
+    
+    draw_status()
     
