2c4d9143d729817bfbccb7b0a87dbc4d4eb66da7
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2021-07-30 10:03:48 +0430

    initialize pygame window

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame as py, sys
+import pygame as pg, sys
 from pygame.locals import *
 import time
 
@@ -13,3 +13,9 @@ line_color = (10, 10, 10)
 # Tic Tac Toe 3*3 board
 TTT = [[None]*3, [None]*3, [None]*3]
 
+# Initialize pygame window.
+pg.init()
+fps = 30
+CLOCK = pg.time.Clock()
+screen = pg.display.set_mode((width, height + 100), 0, 32)
+pg.display.set_caption("Tic Tac Toe")
